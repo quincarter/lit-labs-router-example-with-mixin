67245cc69aa13a1baa03413eb1cfd8adebb23593
Quin Carter <quin.carter@gmail.com>
2024-09-12 18:12:12 -0400

    Removed IRouter

--- src/my-element.ts
+++ src/my-element.ts
@@ -1,5 +1,5 @@
 import { LitElement, css, html } from "lit";
-import { customElement, state } from "lit/decorators.js";
+import { customElement } from "lit/decorators.js";
 import { RouterMixin } from "./RouterMixin";
 
 /**
